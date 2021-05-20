181d651590d1a16c3bfe32295de6ed3120c85b88
sharmilasudarshan <sharmila.sudarshan2@gmail.com>
2021-05-20 15:25:06 +0530

    Create nodes.groovy

---
+++ job-dsl/nodes.groovy
@@ -0,0 +1,17 @@
+job('NodeJS example'){
+  scm{
+    git('git://github.com/sharmilasudarshan/docker-demo.git'){ node -> 
+      node / gitConfigName('DSL User')
+      node / gitConfigEmail('jenkins-dsl@newtech.academy')
+    }
+  }
+  triggers{
+    scm('H/5 * * * *')
+  }
+  wrappers{
+    nodejs('nodejs')
+  }
+  steps{
+    shell("npm install")
+  }
+}
